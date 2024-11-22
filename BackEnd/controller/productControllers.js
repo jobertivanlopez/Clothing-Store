50406d8279e8169abb8ffe47eeef0807a0dc241b
jemarc#27 <delarosajemarc3@gmail.com>
2024-11-22 13:34:37 +0800

    backendSaInventory

---
+++ BackEnd/controller/productControllers.js
@@ -0,0 +1,79 @@
+// controllers/productController.js
+const productRoutes = require('../routes/product');
+
+// Get all product items
+const getProducts = async (req, res) => {
+    try {
+        const product = await Product.find().sort({ createdAt: -1 });  
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Get a single [product] item by ID
+const getProduct = async (req, res) => {
+    const { id } = req.params;  
+
+    try {
+        const product = await Product.findById(id);  
+        if (!clothing) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Create a new product item
+const createProduct = async (req, res) => {
+    const { id, name, stocks, month } = req.body;  
+
+    try {
+        const product = await Product.create({ id, name, stocks, month });  
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Delete a product item by ID
+const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const product = await Product.findByIdAndDelete(id);  
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.status(200).json({ message: '   Product deleted successfully' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Update a product item by ID
+const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const { name, stocks, month } = req.body;
+
+    try {
+        const product = await Product.findByIdAndUpdate(id, { name, stocks, month }, { new: true });  
+        if (!clothing) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Exporting the functions to be used in routes
+module.exports = {
+    getProducts,
+    getProduct,
+    createProduct,
+    deleteProduct,
+    updateProduct
+};
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,1 +1,24 @@
+require('dotenv').config()
 
+const express = require('express')
+const productRoutes = require('./routes/product.js');
+
+
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.use('/api/products',productRoutes) 
+
+
+//listen for requests
+app.listen(process.env.Port, () =>{
+    console.log('Listening on port', process.env.Port)
+})
